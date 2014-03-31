46995105226eb1292ad85f4e351354b17118f21e
Christan Grant <christangrant@gmail.com>
2014-03-31 17:23:31 -0400

    added a KBA serber

--- code/build.sbt
+++ code/build.sbt
@@ -8,13 +8,21 @@ scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
 
 resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
 
+resolvers += "akka" at "http://repo.akka.io/snapshots"
+
 resolvers += "spray repo" at "http://repo.spray.io"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases"
 
-libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.2.3"
 
-libraryDependencies += "com.typesafe.akka" % "akka-remote_2.10" % "2.2.3"
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.3.0"
+
+libraryDependencies += "com.typesafe.akka" % "akka-remote_2.10" % "2.3.0"
+
+libraryDependencies += "com.typesafe.akka" % "akka-agent_2.10" % "2.3.0"
+
+libraryDependencies += "com.typesafe.akka" % "akka-slf4j_2.10" % "2.3.0"
+
 
 libraryDependencies += "org.tukaani" % "xz" % "1.2"
 
@@ -26,5 +34,12 @@ libraryDependencies += "org.apache.thrift" % "libthrift" % "0.9.1"
 
 libraryDependencies += "org.apache.commons" % "commons-compress" % "1.7"
 
+libraryDependencies += "com.typesafe.akka" % "akka-kernel_2.10" % "2.3.0"
+
+libraryDependencies += "com.typesafe.akka" % "akka-camel_2.10" % "2.3.0"
+
+libraryDependencies += "org.apache.camel" % "camel-jetty" % "2.3.0"
+
+libraryDependencies += "org.apache.camel" % "camel-core" % "2.3.0"
 
 
---
+++ code/src/main/scala/edu/ufl/cise/dsr/server/MyCamel.scala
@@ -0,0 +1,67 @@
+
+package edu.ufl.cise.dsr.server
+
+
+import akka.actor.Actor
+import akka.actor.{ActorSystem, Props}
+import akka.actor.Actor._
+//import akka.camel._
+import org.apache.camel.Message
+//import org.apache.camel.Consumer
+//import akka.camel.Message
+//import org.apache.camel.CamelServiceManager
+import org.apache.camel._
+
+
+import akka.camel.CamelExtension
+import akka.camel.{ CamelMessage, Consumer }
+
+import  edu.ufl.cise.dsr.MyLogging
+
+import edu.ufl.cise.dsr.ingestor.KBAIngestor
+
+class MyCamel extends Actor with Consumer {
+  def endpointUri = "jetty:http://0.0.0.0:32608/kba2013"
+
+  val ingestor = new KBAIngestor()
+
+  def receive = {
+    // URL Format: http://0.0.0.0:32608/kba2013?<number>
+    case CamelMessage(body, headers) => sender ! process(headers)
+    case _ => sender ! "No message."
+
+  }
+
+  def process(headers: Map[String, Any]): String = {
+
+    val sb = new StringBuilder
+
+    var thecount = Integer.parseInt(headers("CamelHttpQuery").toString, 10)
+    for(i <- 0 until thecount) {
+      sb ++= ingestor.next.toString
+    }
+
+    sb.toString
+  }
+
+}
+
+
+
+object MyCamel extends MyLogging {
+  def main(args: Array[String]) {
+    val system = ActorSystem("CamelSystem")
+    val camel = CamelExtension(system)
+    val mina = system.actorOf(Props[MyCamel])
+
+
+    //camel.context.addRoutes(MyCamel)
+    /*val service = CamelServiceManager.startCamelService
+    service.awaitEndpointActivationn(1) {
+      Actor.actorOf[Camel].start
+
+    }*/
+  }
+}
+
+
